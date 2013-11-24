a7fdd36334c10121ce17ab1cca9ed4c283f6dc3a
root <root@debian.co.uk>
2013-11-24 19:50:34 +0000

    Example working

--- example.ml
+++ example.ml
@@ -1,5 +1,5 @@
 open Revision
-
+open Core.Std
 
 module IntIsolatable = Isolate(struct 
                                       type t = int
@@ -11,9 +11,11 @@ module IntRevision = Revise(IntIsolatable)
 
 let () = 
   let r = IntRevision.init () in 
-    let (r1, i1) = IntRevision.create 1 in
+    let (r1, i1) = IntRevision.create r 1 in
       let r2 = IntRevision.fork r1 (fun r -> IntRevision.write r i1 2) in
-        printf (IntRevision.read r1 i1);
-        let r1 = IntRevision.join r1 r2 in
-          printf (IntRevision.read r1 i1)    
+        let Some(v) = (IntRevision.read r1 i1) in
+          printf "%d " v;
+          let r1 = IntRevision.join r1 r2 in
+            let Some(v) = (IntRevision.read r1 i1) in
+            printf "%d" v   
         
