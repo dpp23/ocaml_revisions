1344174134b87eb9459413d33ea8e175ff4d48d5
root <root@debian.co.uk>
2014-02-08 15:41:26 +0000

    Keep exceptions raised inside the fork and raise then at join time

--- revision.ml
+++ revision.ml
@@ -49,7 +49,8 @@ module Make(X:Isolatable) : (Revision with type value = X.t and type isolated = 
         { parent : ((int, Isolated.t, Int.comparator) Map.t);
           self : ((int, Isolated.t, Int.comparator) Map.t);
           written : WrittenSet.t;
-          id : int 
+          id : int ;
+          fork_exn : exn option 
         }
   type t = i Deferred.t  
   type value = X.t
@@ -58,11 +59,15 @@ module Make(X:Isolatable) : (Revision with type value = X.t and type isolated = 
 
   let create parent init =  parent >>| fun parent -> let (isolated, seq) = Isolated.create init parent.id in 
                                                     let k = (Map.add parent.parent ~key:(Isolated.get_id isolated) ~data:isolated) in
-                                                      ({parent =  k; self = k;  written = parent.written; id = seq}, isolated) 
+                                                      ({parent =  k; self = k;  written = parent.written; id = seq; fork_exn = None}, isolated) 
 
-  let fork a f = f a
+  let fork a f = try f a with e -> a >>| fun a -> return {a with fork_exn = Some(e)}
   
-  let join a b = Deferred.both a b >>| fun (a,b) -> let wlist = WrittenSet.elements b.written in
+  let join a b = Deferred.both a b 
+                 >>| fun (a,b) -> 
+                       match a.fork_exn with
+                        |Some(e) -> raise e
+                        |None -> let wlist = WrittenSet.elements b.written in
                                                     let rec join_rec a b wrt = match wrt with
                                                       [] -> a
                                                       |x::xs -> let k = Map.find b.self x and kp = Map.find b.parent x and ka = Map.find a.self x in
@@ -91,7 +96,8 @@ module Make(X:Isolatable) : (Revision with type value = X.t and type isolated = 
                 { self = Map.empty ~comparator:Int.comparator;
                   parent = Map.empty ~comparator:Int.comparator;
                   written = WrittenSet.empty;
-                  id = 0
+                  id = 0;
+                  fork_exn = None
                 }
   let get_revision (res : result) = res >>| fun (a, _) -> a
   let get_isolated (res : result) = res >>| fun (_, b) -> b
